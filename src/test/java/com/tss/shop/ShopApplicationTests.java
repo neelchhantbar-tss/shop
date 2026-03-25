b08c1f9414450483e45eaf111bbc3c935e98ac9c
Neel.Chhantbar <neel.chhantbar@tssconsultancy.com>
2026-03-25 08:15:44 +0530

    initial commit

---
+++ src/test/java/com/tss/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tss.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
